3a660c5d737afc7fdc5335a429853ff99a944170
Jae <lijae3552@gmail.com>
2025-09-19 14:39:53 +0900

    Stomp Jwt Authentication

--- src/main/java/com/example/chatserver/chat/config/StompWebSocketConfig.java
+++ src/main/java/com/example/chatserver/chat/config/StompWebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.example.chatserver.chat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +10,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final StompHandler stompHandler;
+
+    public StompWebSocketConfig(StompHandler stompHandler) {
+        this.stompHandler = stompHandler;
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -27,5 +33,11 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 //        /topic/1형태로 메시지를 수신(subscribe)해야 함을 설정
         registry.enableSimpleBroker("/topic");
 
+    }
+
+    // 웹소켓요청(connect, subscribe, disconnect)등의 요청시에는 http header등 http메시지를 넣어올수 있고, 이를 interceptor를 통해 가로채 토큰등을 검증할수 있음.
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }

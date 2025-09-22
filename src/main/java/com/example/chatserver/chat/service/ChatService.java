e3e191594557dee1eb2a3366a1c788d465f66fc6
Jae <lijae3552@gmail.com>
2025-09-22 17:47:54 +0900

    Group Chat Room Create

--- src/main/java/com/example/chatserver/chat/service/ChatService.java
+++ src/main/java/com/example/chatserver/chat/service/ChatService.java
@@ -13,6 +13,7 @@ import com.example.chatserver.member.domain.Member;
 import com.example.chatserver.member.repository.MemberRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -61,5 +62,23 @@ public class ChatService {
             readStatusRepository.save(readStatus);
         }
 
+    }
+
+    public void createGroupRoom(String chatRoomName){
+        Member member = memberRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName()).orElseThrow(()->new EntityNotFoundException("member cannot be found"));
+
+        // 채팅방 생성
+        ChatRoom chatRoom = ChatRoom.builder()
+                .name(chatRoomName)
+                .isGroupChat("Y")
+                .build();
+        chatRoomRepository.save(chatRoom);
+
+        // 채팅참여자로 개설자를 추가
+        ChatParticipant chatParticipant = ChatParticipant.builder()
+                .chatRoom(chatRoom)
+                .member(member)
+                .build();
+        chatParticipantRepository.save(chatParticipant);
     }
 }
